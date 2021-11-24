73b33af07e8dae0850b177c4948d6acd0032ab3d
Виктор Халявин <dartalum@mail.ru>
2021-11-24 23:54:30 +0300

    add

---
+++ index.js
@@ -0,0 +1,8 @@
+const myModal = $.modal({
+    title: 'Viktor model',
+    closable: true,
+    content: `
+    <h4>Mofal is working</h4>
+    <p>Loren ipsum dolor sit.</p>`,
+    width: '400px'
+});
